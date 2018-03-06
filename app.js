35ec562f14b1eab479948fac98f6dfdbe8c32fb8
Geena Gao <geena.gao@gmail.com>
2018-03-06 11:47:14 -0500

    Commit after rendering the template

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require( 'express' );
+const nunjucks = require( 'nunjucks' );
 const app = express();
 
 app.use(function(req, res, next) {
@@ -11,8 +12,37 @@ app.use('/special/', function(req, res, next){
   next();
 });
 
-app.get('/', ((req, res, next) => res.send('Welcome!')));
+app.get('/', ((req, res, next) => res.render( 'index', {title: 'Hall of Fame', people: people} )));
+
+	// res.send('Welcome!')));
 
 app.listen(3000, function(){
   console.log('server listening');
 })
+
+
+// Nunjucks
+
+// nunjucks.configure('views', {noCache: true});
+
+// nunjucks.render('index.html', locals, function (err, output) {
+// 	console.log(output);
+// });
+
+// var localVars = {
+// 	title: 'An Example',
+// 	people : [ 
+// 		{name: Gandalf}, 
+// 		{name: Frodo}, 
+// 		{name: Hermione} 
+// 	]
+// };
+
+// Integration
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', { noCache: true });
+
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+// res.render( 'index', {title: 'Hall of Fame', people: people} );
