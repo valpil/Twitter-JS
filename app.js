f678004e1f945a5aee4d08e4c49e83c8362b4cd8
Val Pilossof <valpil610@gmail.com>
2018-03-06 11:09:19 -0500

    through step 3

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require( 'express' );
 const app = express();
 
+app.use(function(req, res, next) {
+  console.log(req.method, req.originalUrl, res.statusCode);
+  next();
+});
+
+app.use('/special/', function(req, res, next){
+  console.log('You are special, congrats!');
+  next();
+});
+
 app.get('/', ((req, res, next) => res.send('Welcome!')));
 
 app.listen(3000, function(){
